0b9ccca0068d4f6e4924a5d63c6951886f67b8e2
senorgit <srdonline@gmail.com>
2020-03-03 22:18:11 -0800

    all engr478 projects

---
+++ my_toggle/Objects/my_toggle.d
@@ -0,0 +1,5 @@
+.\objects\my_toggle.o: my_toggle.c
+.\objects\my_toggle.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\my_toggle.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\objects\my_toggle.o: ..\..\..\..\inc/tm4c123gh6pm.h
+.\objects\my_toggle.o: ..\..\..\..\driverlib/sysctl.h
---
+++ switch/switch.d
@@ -0,0 +1,3 @@
+.\switch.o: switch.c
+.\switch.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\switch.o: ..\..\..\..\inc/tm4c123gh6pm.h
